117dcdeea15345314536aea2ff5228d28888e1c4
Wayne Werner <wayne@waynewerner.com>
2013-02-24 21:44:39 -0600

    Got blocks drawing with the new version of stuff

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,115 +1,119 @@
-var one = [[1]];
-var two = [[1,1]];
-var vee_three = [[1, 1],
-                 [0, 1]];
-var el_three = [[1, 1, 1]];
-var tee_four = [[0, 1, 0],
-                [1, 1, 1]];
-var oh = [[1, 1],
-          [1, 1]];
-var big_el_four = [[1, 0 ,0],
-                   [1, 1, 1]];
-var el_four = [[1, 1, 1, 1]];
-var zee_four = [[1, 0],
-                [1, 1],
-                [0, 1]];
-var eff = [[0, 1, 0],
-           [1, 1, 1],
-           [1, 0, 0]];
-var eggs = [[0, 1, 0],
-            [1, 1, 1],
-            [0, 1, 0]];
-var pee = [[1, 1],
-           [1, 1],
-           [1, 0]];
-var double_ewe = [[1, 0, 0],
-                  [1, 1, 0],
-                  [0, 1, 1]];
-var zee_five = [[1, 0, 0],
-                [1, 1, 1],
-                [0, 0, 1]];
-var why = [[1, 1, 1, 1],
-           [0, 1, 0, 0]];
-var big_el_five = [[1, 1, 1, 1],
-                   [1, 0, 0, 0]];
-var ewe = [[1, 1, 1],
-           [1, 0, 1]];
-var tee_five = [[0, 1, 0],
-                [0, 1, 0],
-                [1, 1, 1]];
-var vee_five = [[1, 0, 0],
-                [1, 0, 0],
-                [1, 1, 1]];
-var en = [[0, 1, 1, 1],
-          [1, 1, 0, 0]];
-var el_five = [[1, 1, 1, 1, 1]];
+var blocks = [
+    [
+     "One Piece",
+     [[1]] 
+    ],
+    [
+     "Two Piece",
+     [[1, 1]]
+    ],
+    [
+     "Three Piece",
+     [[1, 1, 1]]
+    ],
+    [
+     "Four Piece",
+     [[1, 1, 1, 1]]
+    ],
+    [
+     "Five Piece",
+     [[1, 1, 1, 1, 1]]
+    ],
+    [
+     "Small Corner",
+     [[1, 1],
+      [0, 1]]
+    ],
+    [
+     "Big Corner",
+     [[1, 1, 1],
+      [0, 0, 1],
+      [0, 0, 1]]
+    ],
+    [
+     "Little Tee",
+     [[1, 1, 1],
+      [0, 1, 0]],
+    ],
+    [
+     "Big Tee",
+     [[1, 1, 1],
+      [0, 1, 0],
+      [0, 1, 0]]
+    ],
+    [
+     "Long Tee",
+     [[1, 1, 1, 1],
+      [0, 1, 0, 0]]
+    ], 
+    [
+     "Block",
+     [[1, 1],
+      [1, 1]]
+    ],
+    [
+     "Short Ell",
+     [[1, 0, 0],
+      [1, 1, 1]]
+    ], 
+    [
+     "Long Ell",
+     [[1, 0, 0, 0],
+      [1, 1, 1, 1]]
+    ], 
+    [
+     "S Piece",
+     [[0, 1, 1],
+      [1, 1, 0]]
+    ], 
+    [
+     "Long S Piece",
+     [[0, 1, 1, 1],
+      [1, 1, 0, 0]]
+    ], 
+    [
+     "Big S Piece",
+     [[1, 0, 0],
+      [1, 1, 1],
+      [0, 0, 1]]
+    ], 
+    [
+     "Weird F Piece",
+     [[0, 1, 0],
+      [1, 1, 1],
+      [1, 0, 0]]
+    ], 
+    [
+     "Plus",
+     [[0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0]]
+    ], 
+    [
+     "Turtle",
+     [[1, 1, 1],
+      [0, 1, 1]]
+    ], 
+    [
+     "Zig-Zag",
+     [[1, 0, 0],
+      [1, 1, 0],
+      [0, 1, 1]]
+    ],
+    [
+     "Hat",
+     [[1, 1, 1],
+      [1, 0, 1]]
+    ]
+];
 
-var table = document.createElement("table");
-table.style.borderCollapse = "collapse";
-
-function add_shape(shape, table, color, bordercolor){
-    color = color || 'blue';
-    bordercolor = bordercolor || "lightblue";
-    for (var row = 0;  row < shape.length; row++){
-        var row_el = document.createElement("tr");
-        for (var col = 0; col < shape[row].length; col++){
-            var col_el = document.createElement("td");
-            col_el.style.width = "20px";
-            col_el.style.height = "20px";
-            if (shape[row][col]){
-                col_el.style.backgroundColor = color;
-                col_el.style.borderColor = bordercolor;
-                col_el.style.borderStyle = "solid";
-                col_el.style.borderWidth = "1";
-            }
-            row_el.appendChild(col_el);
-        }
-        table.appendChild(row_el);
-    }
-}
-
-var shapes = [
-     ["One Piece", one],
-     ["Two Piece", two ],
-     ["Three L", vee_three ],
-     ["Three Piece", el_three ],
-     ["Small T", tee_four ],
-     ["Block", oh ],
-     ["Little L", big_el_four ],
-     ["Four Piece", el_four ],
-     ["S Piece", zee_four ],
-     ["Weird F Piece", eff ],
-     ["Plus", eggs ],
-     ["Turtle", pee ],
-     ["Zig-zag", double_ewe ],
-     ["Big S", zee_five ],
-     ["That one piece", why ],
-     ["Long L", big_el_five ],
-     ["Hat", ewe ],
-     ["Big T", tee_five ],
-     ["Big L", vee_five ],
-     ["Long S", en ],
-     ["Five Piece", el_five ],
-     ];
 
 function do_it(){
-    var s = Shape("one", [[1]]);
-    var s = Shape("Zig-Zag", [[1, 0, 0],
-                              [1, 1, 0],
-                              [0, 1, 1]]);
-    document.getElementById("blocks").appendChild(s.table());
-
-    /*
-    for(var i = 0; i < shapes.length; i++){
-        var table = document.createElement("table");
-
-        var heading = document.createElement('h3');
-        heading.innerText = shapes[i][0];
-        document.getElementById("blocks").appendChild(heading);
-
-        table.style.borderCollapse = "collapse";
-        add_shape(shapes[i][1], table, 'red', 'pink');
-        document.getElementById("blocks").appendChild(table);
+    var shapes = [];
+    for(var i = 0; i < blocks.length; i++){
+        var shape = Shape(blocks[i][0],
+                          blocks[i][1]);
+        shapes.push(shape);
+        document.getElementById("blocks").appendChild(shape.table());
     }
-    */
 }
